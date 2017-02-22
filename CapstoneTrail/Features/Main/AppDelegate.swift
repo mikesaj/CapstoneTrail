c41495c2b08f3ad07d54290af205ea63a0d08a29
Joohyung Ryu <kalpafilms@hotmail.com>
2017-02-22 10:22:56 -0500

    Prepare for using Core Data

--- CapstoneTrail/Features/Main/AppDelegate.swift
+++ CapstoneTrail/Features/Main/AppDelegate.swift
@@ -10,6 +10,8 @@ import UIKit
 import Firebase
 import FBSDKCoreKit
 import GoogleSignIn
+import CoreData
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
@@ -17,104 +19,136 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
     var window: UIWindow?
 
 
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-    
         FIRApp.configure()
-        
+
         GIDSignIn.sharedInstance().clientID = FIRApp.defaultApp()?.options.clientID
         GIDSignIn.sharedInstance().delegate = self
-        
-        FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
-        
+
+        FBSDKApplicationDelegate.sharedInstance()
+                                .application(application, didFinishLaunchingWithOptions: launchOptions)
+
         return true
     }
-    
-    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
-        
-        let handled = FBSDKApplicationDelegate.sharedInstance().application(app, open: url, sourceApplication: options [UIApplicationOpenURLOptionsKey.sourceApplication] as! String!, annotation: options[UIApplicationOpenURLOptionsKey.annotation])
-        
-        GIDSignIn.sharedInstance().handle(url,
-                                          sourceApplication: options [UIApplicationOpenURLOptionsKey.sourceApplication] as! String!,
-                                          annotation: options[UIApplicationOpenURLOptionsKey.annotation])
-        
+
+
+    func application(_ app: UIApplication,
+                     open url: URL,
+                     options: [UIApplicationOpenURLOptionsKey: Any] = [:]) -> Bool {
+
+        let handled = FBSDKApplicationDelegate.sharedInstance()
+                                              .application(app, open: url, sourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String!, annotation: options[UIApplicationOpenURLOptionsKey.annotation])
+
+        GIDSignIn.sharedInstance()
+                 .handle(url,
+                         sourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String!,
+                         annotation: options[UIApplicationOpenURLOptionsKey.annotation])
+
         return handled;
     }
-    
+
+
     func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
-        
+
         if let err = error {
             print("Error to log into Google: ", err)
             return
         }
-        
+
         print("Successfully logged into Google", user)
-        
+
         guard let idToken = user.authentication.idToken else { return }
         guard let accessToken = user.authentication.accessToken else { return }
-        
+
         let credentials = FIRGoogleAuthProvider.credential(withIDToken: idToken, accessToken: accessToken)
-        
-        FIRAuth.auth()?.signIn(with: credentials, completion: { (user, error) in
-            
+
+        FIRAuth.auth()?.signIn(with: credentials, completion: {
+            (user, error) in
+
             if let err = error {
                 print("Error to create a Firebase User with Google account: ", err)
                 return
             }
-            
+
             guard let uid = user?.uid else { return }              // For client-side use only!
-            
+
 
             print("Successfully logged into Firebase with Google", uid)
-            
+
             // guard let idToken = user?.refreshToken else { return } // Safe to send to the server
             guard let name = user?.displayName else { return }
             guard let email = user?.email else { return }
-            
+
             print("Welcome: \n \(uid) \n \(name) \n \(email)")
-            
+
             // self.window = UIWindow(frame: UIScreen.main.bounds)
             // self.window?.makeKeyAndVisible()
             // self.window?.rootViewController = UINavigationController(rootViewController: ViewController())
-            
+
 
             //switch view to DashBoard Storyboard
             let dashboard = LoginViewController()
             dashboard.switchToDashBoardView()
-
         })
-        
     }
 
 
     func applicationWillResignActive(_ application: UIApplication) {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
 
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
 
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
 
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
 
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
 
     func applicationWillTerminate(_ application: UIApplication) {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+    // MARK: - Core Data stack
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "KWTrails")
+        container.loadPersistentStores(completionHandler: {
+            (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
 
+    // MARK: - Core Data Saving support
+    func saveContext() {
 
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        }
+    }
 }
