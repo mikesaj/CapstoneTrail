640590ca0a21b9663695badcaddbd431923c24a3
Joohyung Ryu <kalpafilms@hotmail.com>
2017-02-22 10:55:56 -0500

    Display trail data on table cells

--- CapstoneTrail/Features/TrailList/TrailListTableViewController.swift
+++ CapstoneTrail/Features/TrailList/TrailListTableViewController.swift
@@ -7,89 +7,72 @@
 //
 
 import UIKit
+import CoreData
+
 
 class TrailListTableViewController: UITableViewController {
 
+    // MARK: Variables
+    var trails: [NSManagedObject] = []
+
     override func viewDidLoad() {
+
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    override func viewWillAppear(_ animated: Bool) {
+
+        super.viewWillAppear(animated)
+
+        // Setup for fetch
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Trail")
+
+        do {
+            trails = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            debugPrint("Could not fetch. \(error), \(error.userInfo)")
+        }
     }
+
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+
+        return 1
     }
+
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+
+        return trails.count
     }
 
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
-        // Configure the cell...
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        // Prepare for table cell data
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "TrailTableViewCell", for: indexPath) as? TrailTableViewCell else {
+            fatalError("The dequeued cell is not an instance of TrailTableViewCell")
+        }
+        let trail = trails[indexPath.row]
+
+        // Shorten strings
+        let areaName = trail.value(forKey: "area") as! String
+        let streetNameString = NSLocalizedString("StreetName", comment: "Street name near the trail")
+        let trailBriefString = NSLocalizedString("TrailBrief", comment: "Brief data of the trail")
+        let streetName = trail.value(forKey: "street") as! String
+        let length = trail.value(forKey: "length") as! Double
+        let trailType = trail.value(forKey: "pathType") as! String
+
+        // Assign values
+        cell.areaLogoImage.image = UIImage(named: areaName)
+        cell.streetNameLabel.text = String(format: streetNameString, streetName)
+        cell.trailBriefLabel.text = String(format: trailBriefString, length, trailType.capitalized)
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
