33a0d8a30992988a6890bf19c59fb0c15a9d66e5
Joohyung Ryu <kalpafilms@hotmail.com>
2017-03-04 12:35:04 -0500

    Add TrailMap storyboard, ViewController, and unit tests files

---
+++ CapstoneTrail/Features/TrailMapViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TrailMapViewController.swift
+//  CapstoneTrail
+//
+//  Created by Joohyung Ryu on 2017. 3. 4..
+//  Copyright (c) 2017 MSD. All rights reserved.
+//
+
+import UIKit
+
+
+class TrailMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+
+        super.viewDidLoad()
+    }
+}
---
+++ CapstoneTrailTests/TrailMapViewControllerTests.swift
@@ -0,0 +1,25 @@
+//
+//  TrailMapViewControllerTests.swift
+//  CapstoneTrail
+//
+//  Created by Joohyung Ryu on 2017. 3. 4..
+//  Copyright (c) 2017 MSD. All rights reserved.
+//
+
+import XCTest
+@testable import CapstoneTrail
+
+
+class TrailMapViewControllerTests: XCTestCase {
+
+    override func setUp() {
+
+        super.setUp()
+    }
+
+
+    override func tearDown() {
+
+        super.tearDown()
+    }
+}
